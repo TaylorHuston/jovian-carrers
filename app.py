92d18e9ae91ce4dbb9b0e83400184d298b50bf9f
Taylor Huston <taylor.a.huston@gmail.com>
2024-03-03 22:11:25 -0500

    Refactor and include jobs in app

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
 from flask import Flask, jsonify, render_template
+from database import load_jobs
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Software Engineer',
-        'location': 'San Francisco, CA',
-        'salary': '$100,000 - $150,000',
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'New York, NY',
-        'salary': '$120,000 - $180,000',
-    },
-    {
-        'id': 3,
-        'title': 'Product Manager',
-        'location': 'Seattle, WA',
-        'salary': '$130,000 - $200,000',
-    },
-    {
-        'id': 4,
-        'title': 'DevOps Engineer',
-        'location': 'Austin, TX',
-    },
-]
+# JOBS = [
+#     {
+#         'id': 1,
+#         'title': 'Software Engineer',
+#         'location': 'San Francisco, CA',
+#         'salary': '$100,000 - $150,000',
+#     },
+#     {
+#         'id': 2,
+#         'title': 'Data Scientist',
+#         'location': 'New York, NY',
+#         'salary': '$120,000 - $180,000',
+#     },
+#     {
+#         'id': 3,
+#         'title': 'Product Manager',
+#         'location': 'Seattle, WA',
+#         'salary': '$130,000 - $200,000',
+#     },
+#     {
+#         'id': 4,
+#         'title': 'DevOps Engineer',
+#         'location': 'Austin, TX',
+#     },
+# ]
+
 
 @app.route("/")
 def hello_world():
+    all_jobs = load_jobs()
     return render_template('home.html', 
-                           jobs=JOBS,
+                           jobs=all_jobs,
                            company_name='Jovian')
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    all_jobs = load_jobs()
+    return jsonify(all_jobs)
 
 # If ran from the command line with python3 app.py
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-import sqlalchemy
 from sqlalchemy import create_engine, text
 
 # Bad that this is in plain text, following along with tutorial though
@@ -11,19 +10,23 @@ engine = create_engine(db_connection_string,
                            }
                        })
 
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs"))
-    print(type(result))
-    print(result)
-    result_all = result.all()
-    print(type(result_all))
-    print(result_all)
-    print(type(result_all[0]))
-    print(dict(result_all[0]))
+# with engine.connect() as conn:
+#     result = conn.execute(text("SELECT * FROM jobs"))
+#     print(type(result))
+#     print(result)
+#     result_all = result.all()
+#     print(type(result_all))
+#     print(result_all)
+#     print(type(result_all[0]))
+#     print(dict(result_all[0]))
 
-    result_all_dicts = [dict(row) for row in result_all]
+#     result_all_dicts = [dict(row) for row in result_all]
 
-    print(result_all_dicts)
+#     print(result_all_dicts)
 
- 
+def load_jobs():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
 
+        jobs = [dict(row) for row in result.all()]
+    return jobs
